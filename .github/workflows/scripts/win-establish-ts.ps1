b74f70ce9968e2bf149da07de28521f8e5d483ce
cicybot <cicybot@outlook.com>
2026-01-06 16:56:56 +0630

    Update win-establish-ts.ps1

--- .github/workflows/scripts/win-establish-ts.ps1
+++ .github/workflows/scripts/win-establish-ts.ps1
@@ -1,14 +1,18 @@
 # Bring up Tailscale and get IP
 
-& "$env:ProgramFiles\Tailscale\tailscale.exe" up --authkey=$env:TAILSCALE_AUTH_KEY --hostname=gh-runner-$env:GITHUB_RUN_ID
+# & "$env:ProgramFiles\Tailscale\tailscale.exe" up --authkey=$env:TAILSCALE_AUTH_KEY --hostname=gh-runner-$env:GITHUB_RUN_ID
+
+# $tsIP = $null
+# $retries = 0
+# while (-not $tsIP -and $retries -lt 10) {
+#   $tsIP = & "$env:ProgramFiles\Tailscale\tailscale.exe" ip -4
+#   Start-Sleep -Seconds 5
+#   $retries++
+# }
+# if (-not $tsIP) { throw "Tailscale IP not assigned." }
+# echo "TAILSCALE_IP=$tsIP" >> $env:GITHUB_ENV
 
 
-$tsIP = $null
-$retries = 0
-while (-not $tsIP -and $retries -lt 10) {
-  $tsIP = & "$env:ProgramFiles\Tailscale\tailscale.exe" ip -4
-  Start-Sleep -Seconds 5
-  $retries++
-}
-if (-not $tsIP) { throw "Tailscale IP not assigned." }
-echo "TAILSCALE_IP=$tsIP" >> $env:GITHUB_ENV
+& "$env:ProgramFiles\Cloudflared\cloudflared.exe service install $env:TAILSCALE_AUTH_KEY
+
+
