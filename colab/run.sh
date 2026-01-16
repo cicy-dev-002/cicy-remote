6aeec70b403003a5aeab73ca9bdd47478df5a620
cicybot <cicybot@outllok.com>
2026-01-16 23:01:57 +0630

    refact code

--- colab/run.sh
+++ colab/run.sh
@@ -1,1 +1,10 @@
-echo run...
+
+if [ ! -d "/content/cloudflare-python-workers" ]; then
+    echo "Cloning cloudflare-python-workers..."
+    git clone https://github.com/cicybot/cloudflare-python-workers.git /content/cloudflare-python-workers
+else
+    cd /content/cloudflare-python-workers
+    git pull origin main
+fi
+cd cd /content/cloudflare-python-workers/workers
+ls -alh
