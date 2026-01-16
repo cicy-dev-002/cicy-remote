040f3cfa7a5c3fa422a8ac9ca44096473179e593
cicybot <cicybot@outllok.com>
2026-01-16 23:35:50 +0630

    refact code

--- colab/run.sh
+++ colab/run.sh
@@ -12,7 +12,7 @@ cd workers
 export API_URL=https://mac-8989.cicy.de5.net
 #
 uv sync
-#pkill worker.py
-#nohup uv run worker.py colab_1001  > /content/workers.log 2>&1 &
-#sleep 1
-#cat /content/workers.log
+pkill worker.py
+nohup uv run worker.py colab_1001  > /content/workers.log 2>&1 &
+sleep 2
+cat /content/workers.log
