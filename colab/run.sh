13e356fb338f4370dad6c42b7faf0fb9ba2fb157
cicybot <cicybot@outllok.com>
2026-01-16 23:20:54 +0630

    refact code

--- colab/run.sh
+++ colab/run.sh
@@ -1,4 +1,3 @@
-rm -rf /content/cloudflare-python-workers
 if [ ! -d "/content/cloudflare-python-workers" ]; then
     echo "Cloning cloudflare-python-workers..."
     git clone https://github.com/cicybot/cloudflare-python-workers.git /content/cloudflare-python-workers
@@ -10,8 +9,9 @@ else
 fi
 cd workers
 export API_URL=https://mac-8989.cicy.de5.net
-uv sync
-pkill worker.py
-nohup uv run worker.py colab_1001  > /content/workers.log 2>&1 &
-sleep 1
-cat /content/workers.log
+#
+#uv sync
+#pkill worker.py
+#nohup uv run worker.py colab_1001  > /content/workers.log 2>&1 &
+#sleep 1
+#cat /content/workers.log
