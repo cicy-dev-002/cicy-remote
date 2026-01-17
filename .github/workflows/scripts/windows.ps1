5af7d3e6f04d9b229fa46efab9bbd4be3343d952
cicybot <cicybot@outllok.com>
2026-01-17 17:47:11 +0630

    refact code

--- .github/workflows/scripts/windows.ps1
+++ .github/workflows/scripts/windows.ps1
@@ -84,3 +84,19 @@ Remove-Item $installerPath -Force
 # Establish Cloudflared Connection
 & "C:\Program Files (x86)\cloudflared\cloudflared.exe" service install $env:CF_TUNNEL
 
+pip install jupyterlab
+jupyter --version
+# Run Jupyter Lab in background (detached)
+Start-Process `
+-FilePath "jupyter" `
+-ArgumentList @(
+  "lab",
+  "--IdentityProvider.token=$env:JUPYTER_TOKEN",
+  "--ip=0.0.0.0",
+  "--port=8888",
+  "--ServerApp.allow_remote_access=True",
+  "--ServerApp.trust_xheaders=True",
+  "--no-browser"
+) `
+-WindowStyle Hidden
+
