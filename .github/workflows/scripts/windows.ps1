d4086f436bfa239806ae385553b3f574def3b5cf
cicybot <cicybot@outllok.com>
2026-01-19 11:58:41 +0630

    refact code

--- .github/workflows/scripts/windows.ps1
+++ .github/workflows/scripts/windows.ps1
@@ -107,7 +107,11 @@ Write-Host "Establishing Cloudflared connection..."
 & "C:\Program Files (x86)\cloudflared\cloudflared.exe" service install $env:CF_TUNNEL
 Write-Host "Cloudflared service installed."
 
-Start-Process "C:\Program Files (x86)\cloudflared\cloudflared.exe" -ArgumentList "access smb --hostname gcs-smb.cicy.de5.net --url 127.0.0.1:445" -WindowStyle Hidden; Start-Sleep 2; Get-Process cloudflared
+
+Start-Process "C:\Program Files (x86)\cloudflared\cloudflared.exe" -ArgumentList "access smb --hostname gcs-smb.cicy.de5.net --url 127.0.0.1:445"
+
+# Start-Process "C:\Program Files (x86)\cloudflared\cloudflared.exe" -ArgumentList "access smb --hostname gcs-smb.cicy.de5.net --url 127.0.0.1:445" -WindowStyle Hidden; Start-Sleep 2; Get-Process cloudflared
+
 #
 # pip install pyautogui pyperclip
 #
