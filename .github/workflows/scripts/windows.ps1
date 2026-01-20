77f2427f57c61b1a0f0b089099ce570b1317c535
cicybot <cicybot@outllok.com>
2026-01-20 10:44:26 +0630

    refact code

--- .github/workflows/scripts/windows.ps1
+++ .github/workflows/scripts/windows.ps1
@@ -108,10 +108,6 @@ Write-Host "Establishing Cloudflared connection..."
 Write-Host "Cloudflared service installed."
 
 
-Start-Process "C:\Program Files (x86)\cloudflared\cloudflared.exe" -ArgumentList "access smb --hostname gcs-smb.cicy.de5.net --url 127.0.0.1:4445" -WindowStyle Hidden; Start-Sleep 2; Get-Process cloudflared
-
-
-
 # =========================================================
 # One-Key Cloudflared SMB → Z: (Windows / rclone / WinFsp)
 # =========================================================
@@ -285,3 +281,6 @@ Write-Host "Dependencies installed."
 
 git config --global user.email "GA-WIN@gmail.com"
 git config --global user.name "GA WIN"
+
+
+Start-Process "C:\Program Files (x86)\cloudflared\cloudflared.exe" -ArgumentList "access smb --hostname gcs-smb.cicy.de5.net --url 127.0.0.1:4445"; Start-Sleep 2; Get-Process cloudflared
