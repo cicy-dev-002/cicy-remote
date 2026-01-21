5cdb737c6fa381a40eaed6899e505f239d422bf1
cicybot <cicybot@outllok.com>
2026-01-21 16:08:34 +0630

    refact code

--- gcs/boot.sh
+++ gcs/boot.sh
@@ -1,12 +1,11 @@
 
 bash <(curl -fsSL https://raw.githubusercontent.com/cicybot/cloudflare-tunnel-proxy/refs/heads/main/install-cloudflared.sh)
 
-
 pkill cloudflared
 nohup cloudflared tunnel run --token "$CF_TUNNEL" > ~/tunnel.log 2>&1 &
 
 ps aux | grep cloudflared
-npm instll -g opencode-ai
+npm install -g opencode-ai
 
 grep -qxF "alias oc='~/.opencode/bin/opencode'" ~/.bashrc || echo "alias oc='~/.opencode/bin/opencode'" >> ~/.bashrc
 
