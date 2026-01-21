88000b9813765e96a8f943bcfd7d230fe43f6140
cicybot <cicybot@outllok.com>
2026-01-21 07:47:30 +0630

    refact code

--- .github/workflows/scripts/ubuntu.sh
+++ .github/workflows/scripts/ubuntu.sh
@@ -18,25 +18,27 @@ docker run --name 3proxy --rm -d -p "8082:3128/tcp" ghcr.io/tarampampam/3proxy:1
 docker ps
 
 #install Electron
-npm install electron -g
-git clone https://github.com/cicybot/electron-mcps.git
-cd electron-mcps
-sh electron.sh
-cd run
-Xvfb :1 -screen 0 1280x1024x24 &
-export DISPLAY=:1
-nohup electron  --no-sandbox --disable-software-rasterizer  --disable-gpu   --enable-unsafe-swiftshader   --disable-features=UseDBus   --log-level=3  main.js > ~/electron.log 2>&1 &
-sleep 2
-cat ~/electron.log
-#Install Jupyter and Run Jupyter in background
-pip install --upgrade pip
-pip install jupyterlab
-jupyter --version
-# Run Jupyter Lab in background
-nohup jupyter lab \
-  --ServerApp.token=$JUPYTER_TOKEN \
-  --ip=0.0.0.0 \
-  --port=8888 \
-  --ServerApp.allow_remote_access=True \
-  --ServerApp.trust_xheaders=True \
-  --no-browser > jupyter.log 2>&1 &
+#npm install electron -g
+#git clone https://github.com/cicybot/electron-mcp.git
+#cd electron-mcp
+#sh electron.sh
+#cd run
+#Xvfb :1 -screen 0 1280x1024x24 &
+#export DISPLAY=:1
+#nohup electron  --no-sandbox --disable-software-rasterizer  --disable-gpu   --enable-unsafe-swiftshader   --disable-features=UseDBus   --log-level=3  main.js > ~/electron.log 2>&1 &
+#sleep 2
+#cat ~/electron.log
+##Install Jupyter and Run Jupyter in background
+#pip install --upgrade pip
+#pip install jupyterlab
+#jupyter --version
+## Run Jupyter Lab in background
+#nohup jupyter lab \
+#  --ServerApp.token=$JUPYTER_TOKEN \
+#  --ip=0.0.0.0 \
+#  --port=8888 \
+#  --ServerApp.allow_remote_access=True \
+#  --ServerApp.trust_xheaders=True \
+#  --no-browser > jupyter.log 2>&1 &
+
+sh vnc/install.sh
