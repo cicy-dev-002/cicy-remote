0a7c901c749a92f89135276937a0fd29f7fbf5bf
cicybot <cicybot@outllok.com>
2026-01-22 13:21:10 +0630

    refact code

--- gcs/boot.sh
+++ gcs/boot.sh
@@ -1,11 +1,5 @@
-source
-
 if [ -f ~/gcs-env.sh ]; then
   source ~/gcs-env.sh
-fi
-
-if [ -f /content/gcs-env.sh ]; then
-  source /content/gcs-env.sh
 fi
 
 bash <(curl -fsSL https://raw.githubusercontent.com/cicybot/cloudflare-tunnel-proxy/refs/heads/main/install-cloudflared.sh)
