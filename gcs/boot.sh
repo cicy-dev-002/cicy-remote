7cbc57daef58bcc7504ef3ecee0e845f3fc01e20
Your Name <you@example.com>
2026-01-26 18:08:01 +0000

    no msg

--- gcs/boot.sh
+++ gcs/boot.sh
@@ -7,4 +7,8 @@ bash <(curl -fsSL https://raw.githubusercontent.com/cicybot/cloudflare-tunnel-pr
 pkill cloudflared
 nohup cloudflared tunnel run --token "$CF_TUNNEL" > ~/tunnel.log 2>&1 &
 
+# cd ~/
+# nohup oc web --port 4060 > ~/oc.log 2>&1 &
 ps aux | grep cloudflared
+# ps aux | grep opencode
+
