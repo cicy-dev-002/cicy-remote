9236aec7150f769480e07ac84da7ad823eebf0b6
cicybot <cicybot@outllok.com>
2026-01-31 13:24:11 +0630

    refact code

--- scripts/windows/test.ps1
+++ scripts/windows/test.ps1
@@ -1,2 +1,6 @@
-Write-Host "ENV DATA: $env:DATA"
-python scripts/windows/parse-base64-json.py $env:DATA
+$result = python scripts/windows/parse-base64-json.py $env:DATA
+$json = $result | ConvertFrom-Json
+$env:test1 = $json.test1
+$env:test2 = $json.test2
+echo $env:test1
+echo $env:test2
